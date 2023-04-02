138173bb27f0d0fd85d40721ec10ad6deebe639b
Rafael <rafael.costa.ti@gmail.com>
2023-04-01 22:43:08 -0300

    updated ApiCallInterceptor.kt and MatchesApi.kt

--- app/src/main/java/com/orafaelsc/fuzecodechallenge/data/api/ApiCallInterceptor.kt
+++ app/src/main/java/com/orafaelsc/fuzecodechallenge/data/api/ApiCallInterceptor.kt
@@ -7,10 +7,15 @@ import java.io.IOException
 class ApiCallInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response = try {
         chain.proceed(
-            chain.request().newBuilder().addHeader(
-                name = "x-access-token",
-                value = "YAqDjdon0cKQkfEPOyZrOB8wneXfznhnek8dpBH8g39nmK_QunU",
-            ).build(),
+            chain.request().newBuilder()
+                .addHeader(
+                    name = "Accept",
+                    value = "application/json",
+                )
+                .addHeader(
+                    name = "Authorization",
+                    value = "Bearer YAqDjdon0cKQkfEPOyZrOB8wneXfznhnek8dpBH8g39nmK_QunU",
+                ).build(),
         )
     } catch (e: IOException) {
         throw e
